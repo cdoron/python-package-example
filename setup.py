ff90f93f8e4367f176236851aaa7cced7970e54a
Doron Chen <cdoron@il.ibm.com>
2022-02-03 16:08:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='python-package-example',
+    name='qqq',
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
