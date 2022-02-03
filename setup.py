09f18bf5bae42d9f9b407b16c62ee54bd5e481d0
Doron Chen <cdoron@il.ibm.com>
2022-02-03 16:33:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='qqq',
-    version='v1.0.1',
+    version='v1.0.2',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='An example python package',
     long_description=open('README.txt').read(),
     install_requires=['numpy'],
-    url='https://github.com/BillMills/python-package-example',
+    url='https://github.com/cdoron/qqq',
     author='Bill Mills',
     author_email='myemail@example.com'
 )
